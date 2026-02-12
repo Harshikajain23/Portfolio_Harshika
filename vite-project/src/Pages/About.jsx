76a1994f47d8e77db46d3ebc367423396c3e70a9
Harshikajain23 <jainharshika2002@gmail.com>
2026-02-12 22:20:58 +0530

    Favicon updated

--- vite-project/src/Pages/About.jsx
+++ vite-project/src/Pages/About.jsx
@@ -64,25 +64,25 @@ const About = () => {
       {/* Content */}
       <div className="relative z-10 h-full px-6">
         <div className="mt-10 mb-10 ml-2 sm:ml-2 mr-2 sm:mr-2 lg:ml-15 lg:mr-15 w-70vw h-70vh border border-2 border-white sm:flex flex-col justify-center items-center">
-        <h1 className="text-4xl sm:text-2xl mx-auto text-center font-semibold mt-6 mb-10">About Me</h1>
+        <h1 className="uppercase text-4xl sm:text-2xl lg:text-4xl mx-auto text-center font-semibold mt-6 mb-10">About Me</h1>
 
-        <p className=" text-2xl sm:text-xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
+        <p className=" text-2xl sm:text-xl lg:text-2xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
           Hi, I’m Harshika Jain. I completed my Integrated Master’s degree in Computer Science from the Central University of Rajasthan. During my bachelor’s studies, I built a strong foundation in core computer science subjects, and in my master’s, I specialized in Artificial Intelligence.
         </p>
 
-        <p className=" mt-5 text-2xl sm:text-xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
+        <p className=" mt-5 text-2xl sm:text-xl lg:text-2xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
        In my final year, I developed a deep learning project on Image Captioning using CNN and LSTM architectures, which strengthened my understanding of neural networks and model implementation.
         </p>
 
-        <p className=" mt-5 text-2xl sm:text-xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
+        <p className=" mt-5 text-2xl sm:text-xl lg:text-2xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
           After completing my degree, I focused on enhancing my problem-solving skills through Striver’s DSA sheet and worked extensively on MERN stack development. I have built multiple projects and cloned real-world applications to sharpen my full-stack development skills.
         </p>
 
-        <p className="mt-5 text-2xl sm:text-xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
+        <p className="mt-5 text-2xl sm:text-xl lg:text-2xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
          Beyond academics and coding, I genuinely enjoy solving logical problems, playing chess, reading novels, and watching anime. Chess and problem-solving help sharpen my analytical thinking, while reading and anime inspire creativity and imagination.
         </p>
 
-        <p className="mt-5 mb-6 text-2xl sm:text-xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
+        <p className="mt-5 mb-6 text-2xl sm:text-xl lg:text-2xl text-white ml-4 sm:ml-5 sm:mr-5 lg:ml-15 lg:mr-15 leading-relaxed">
           I am highly motivated, adaptable, and eager to contribute to a team where I can continue learning and make meaningful contributions.
         </p>
 
